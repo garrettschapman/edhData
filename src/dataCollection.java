f9654ceb4e6fa7704969c98b8f6dad545cc483b0
Garrett Chapman <36682034+garrettschapman@users.noreply.github.com>
2020-05-11 17:16:44 -0400

    Fixed dataCollection class to collect all data

--- src/dataCollection.java
+++ src/dataCollection.java
@@ -5,9 +5,6 @@ import java.util.Scanner;
 // collects data from the user for edhData
 
 class dataCollection {
-	// constructs
-	private Scanner in = new Scanner(System.in);
-	
 	// variables
 	private String[][] playerData; // array to hold info about the player
 	private int numPlayers; //number of players
@@ -23,174 +20,175 @@ class dataCollection {
 	 * each "row" in the array is a player
 	 */
 	public void collectData() {
+		Scanner in = new Scanner(System.in); // scanner to get info about game
 		boolean winnerFound = false; // stop asking for a winner if there already is one
 		
 		// loop to go through array
 		for (int i = 0; i < numPlayers; i++) {
-			System.out.println("Player " + i+1 + ":");
+			System.out.println("Player " + (i+1) + ":");
 			
 			// begin questions
 			System.out.print("Name: ");
-			playerData[i][0] = in.next();
+			playerData[i][0] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Commander: ");
-			playerData[i][1] = in.next();
+			playerData[i][1] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Theme: ");
-			playerData[i][2] = in.next();
+			playerData[i][2] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Color identity: ");
-			playerData[i][3] = in.next();
+			playerData[i][3] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of colors: ");
-			playerData[i][4] = in.next();
+			playerData[i][4] = in.nextLine();
 			System.out.println();
 			
-			System.out.print("Did they have fun? y/e/n:");
-			playerData[i][5] = in.next();
+			System.out.print("Did they have fun? y/e/n: ");
+			playerData[i][5] = in.nextLine();
 			System.out.println();
 			
 			// opening hand info
 			System.out.print("Colors in opening hand: ");
-			playerData[i][6] = in.next();
+			playerData[i][6] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Colors of mana in opening hand: ");
-			playerData[i][7] = in.next();
+			playerData[i][7] = in.nextLine();
 			System.out.println();
 			
-			System.out.print("Total CMC of opening hand (not including lands): ");
-			playerData[i][8] = in.next();
+			System.out.print("Total CMC of opening hand: ");
+			playerData[i][8] = in.nextLine();
 			System.out.println();
 			
 			// card types
 			System.out.print("Number of artifacts kept: ");
-			playerData[i][9] = in.next();
+			playerData[i][9] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of creatures kept: ");
-			playerData[i][10] = in.next();
+			playerData[i][10] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of lands kept: ");
-			playerData[i][11] = in.next();
+			playerData[i][11] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of enchantments kept: ");
-			playerData[i][12] = in.next();
+			playerData[i][12] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of instants kept: ");
-			playerData[i][13] = in.next();
+			playerData[i][13] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of sorceries kept: ");
-			playerData[i][14] = in.next();
+			playerData[i][14] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of planeswalkers kept: ");
-			playerData[i][15] = in.next();
+			playerData[i][15] = in.nextLine();
 			System.out.println();
 			
 			// card themes
 			System.out.print("Number of mana cards kept: ");
-			playerData[i][16] = in.next();
+			playerData[i][16] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of draw cards kept: ");
-			playerData[i][17] = in.next();
+			playerData[i][17] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of interaction cards kept: ");
-			playerData[i][18] = in.next();
+			playerData[i][18] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of threat cards kept: ");
-			playerData[i][19] = in.next();
+			playerData[i][19] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of combo cards kept: ");
-			playerData[i][20] = in.next();
+			playerData[i][20] = in.nextLine();
 			System.out.println();
 			
 			System.out.print("Number of other cards kept: ");
-			playerData[i][21] = in.next();
+			playerData[i][21] = in.nextLine();
 			System.out.println();
 			
 			// mulligan info
 			System.out.print("Number of mulligans: ");
-			int mulligans = in.nextInt();
-			playerData[i][22] = Integer.toString(mulligans);
+			playerData[i][22] = in.nextLine();
+			int mulligans = Integer.parseInt(playerData[i][22]);
 			System.out.println();
 			
 			// avoids asking player questions if there aren't mulligans
 			if (mulligans > 0) {
 				System.out.print("Number of cards pitched: ");
-				int pitched = in.nextInt();
-				playerData[i][23] = Integer.toString(pitched);
+				playerData[i][23] = in.nextLine();
+				int pitched = Integer.parseInt(playerData[i][23]);
 				System.out.println();
 				
 				if (pitched > 0) {
-					System.out.print("Total CMC of cards pitched (not including lands): ");
-					playerData[i][24] = in.next();
+					System.out.print("Total CMC of cards pitched: ");
+					playerData[i][24] = in.nextLine();
 					System.out.println();
 					
 					// card types
 					System.out.print("Number of artifacts pitched: ");
-					playerData[i][25] = in.next();
+					playerData[i][25] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of creatures pitched: ");
-					playerData[i][26] = in.next();
+					playerData[i][26] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of lands pitched: ");
-					playerData[i][27] = in.next();
+					playerData[i][27] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of enchantments pitched: ");
-					playerData[i][28] = in.next();
+					playerData[i][28] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of instants pitched: ");
-					playerData[i][29] = in.next();
+					playerData[i][29] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of sorceries pitched: ");
-					playerData[i][30] = in.next();
+					playerData[i][30] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of planeswalkers pitched: ");
-					playerData[i][31] = in.next();
+					playerData[i][31] = in.nextLine();
 					System.out.println();
 					
 					// card themes
 					System.out.print("Number of mana cards pitched: ");
-					playerData[i][32] = in.next();
+					playerData[i][32] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of draw cards pitched: ");
-					playerData[i][33] = in.next();
+					playerData[i][33] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of interaction cards pitched: ");
-					playerData[i][34] = in.next();
+					playerData[i][34] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of threat cards pitched: ");
-					playerData[i][35] = in.next();
+					playerData[i][35] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of combo cards pitched: ");
-					playerData[i][36] = in.next();
+					playerData[i][36] = in.nextLine();
 					System.out.println();
 					
 					System.out.print("Number of other cards pitched: ");
-					playerData[i][37] = in.next();
+					playerData[i][37] = in.nextLine();
 					System.out.println();
 				} else {
 					// for loop to assign 0 to all cards pitched
@@ -204,9 +202,9 @@ class dataCollection {
 					playerData[i][j + 23] = "0";
 				} // end of for loop
 			} // end of checks
-			
+		
 			System.out.print("Did they scoop? y/n: ");
-			playerData[i][38] = in.next();
+			playerData[i][38] = in.nextLine();
 			System.out.println();
 			
 			// avoids asking about winning
@@ -214,34 +212,32 @@ class dataCollection {
 				playerData[i][39] = "n";
 				playerData[i][40] = "-";
 			} else {
-				System.out.print("Did they win? y/n");
-				playerData[i][39] = in.next();
+				System.out.print("Did they win? y/n: ");
+				playerData[i][39] = in.nextLine();
 				System.out.println();
 				
 				// checks if a winner has been found
 				if (playerData[i][39].equals("y")) {
 					winnerFound = true;
 					
-					System.out.print("How did they win? aggro/aether/labman/combo/scoops/other");
-					playerData[i][40] = in.next();
+					System.out.print("How did they win? aggro/aether/labman/combo/scoops/other: ");
+					playerData[i][40] = in.nextLine();
 					System.out.println();
 				} else {
 					playerData[i][40] = "-";
 				} //end of checks
 			} //end of checks
 			
-			if (i++ < numPlayers) {
-				System.out.println();
-				System.out.println();
-				System.out.println();
-				
-				System.out.println("=============================================================================================");
-				
-				System.out.println();
-				System.out.println();
-				System.out.println();
-			}
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("=============================================================================================");
+			System.out.println();
+			System.out.println();
+			System.out.println();
 		} // end of for loop
+		
+		in.close();
 	} // end of function collectData
 	
 	/*
--- src/edhData.java
+++ src/edhData.java
@@ -3,22 +3,27 @@ import java.util.Scanner;
 // edhData class by Garrett Chapman
 // last updated 05/11/2020
 
-class edhData {
+public class edhData {
 	// constructs
-	private static Scanner in = new Scanner(System.in);
 	private static dataCollection collect;
 	
 	// variables
-	private String[][] playerData;
+	private static String[][] playerData;
 	
 	/*
 	 * main method
 	 * creates a data collection object to collect data
 	 */
-	static void main(String[] args) {
+	public static void main(String[] args) {
+		// gets the number of players
+		Scanner in = new Scanner(System.in);
 		System.out.print("How many players? ");
 		int players = in.nextInt();
+		
+		// collects data from the game
 		collect = new dataCollection(players);
 		collect.collectData();
+		playerData = collect.getPlayerData();
+		in.close();
 	} // end of main method
 }
