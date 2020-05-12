b6e635524570fbd6edf3f74332cf6791b0d74d50
Garrett Chapman <36682034+garrettschapman@users.noreply.github.com>
2020-05-11 23:49:16 -0400

    Added functionality to main menu
    Add and exit work, although add has a bug where it displays the menu twice after finishing.

--- src/dataCollection.java
+++ src/dataCollection.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
 // dataCollection class by Garrett Chapman
 // last updated 05/11/2020
 // collects data from the user for edhData
 
 class dataCollection {
-	Scanner in = new Scanner(System.in);
-	
 	// variables
 	private String[][] playerData; // array to hold info about the player
 	private int numPlayers; //number of players
@@ -30,137 +26,132 @@ class dataCollection {
 		// loop to go through array
 		for (int i = 0; i < numPlayers; i++) {
 			System.out.println("Player " + (i+1) + ":");
-			System.out.println();
 			
 			// begin questions
 			System.out.print("Name: ");
-			playerData[i][0] = in.nextLine();
-			System.out.println();
+			playerData[i][0] = edhData.in.nextLine();
 			
 			System.out.print("Commander: ");
-			playerData[i][1] = in.nextLine();
-			System.out.println();
+			playerData[i][1] = edhData.in.nextLine();
 			
 			System.out.print("Theme: ");
-			playerData[i][2] = in.nextLine();
-			System.out.println();
+			playerData[i][2] = edhData.in.nextLine();
 			
 			System.out.print("Color identity: ");
-			playerData[i][3] = in.nextLine();
-			System.out.println();
+			playerData[i][3] = edhData.in.nextLine();
 			
 			System.out.print("Number of colors: ");
-			validateNum(i, 4, in.nextLine());
+			validateNum(i, 4, edhData.in.nextLine());
 			
 			System.out.print("Did they have fun? y/e/n: ");
-			validateText(i, 5, in.nextLine(), yen);
+			validateText(i, 5, edhData.in.nextLine(), yen);
 			
 			// opening hand info
 			System.out.print("Colors in opening hand: ");
-			validateNum(i, 6, in.nextLine());
+			validateNum(i, 6, edhData.in.nextLine());
 			
 			System.out.print("Colors of mana in opening hand: ");
-			validateNum(i, 7, in.nextLine());
+			validateNum(i, 7, edhData.in.nextLine());
 			
 			System.out.print("Total CMC of opening hand: ");
-			validateNum(i, 8, in.nextLine());
+			validateNum(i, 8, edhData.in.nextLine());
 			
 			// card types
 			System.out.print("Number of artifacts kept: ");
-			validateNum(i, 9, in.nextLine());
+			validateNum(i, 9, edhData.in.nextLine());
 			
 			System.out.print("Number of creatures kept: ");
-			validateNum(i, 10, in.nextLine());
+			validateNum(i, 10, edhData.in.nextLine());
 			
 			System.out.print("Number of lands kept: ");
-			validateNum(i, 11, in.nextLine());
+			validateNum(i, 11, edhData.in.nextLine());
 			
 			System.out.print("Number of enchantments kept: ");
-			validateNum(i, 12, in.nextLine());
+			validateNum(i, 12, edhData.in.nextLine());
 			
 			System.out.print("Number of instants kept: ");
-			validateNum(i, 13, in.nextLine());
+			validateNum(i, 13, edhData.in.nextLine());
 			
 			System.out.print("Number of sorceries kept: ");
-			validateNum(i, 14, in.nextLine());
+			validateNum(i, 14, edhData.in.nextLine());
 			
 			System.out.print("Number of planeswalkers kept: ");
-			validateNum(i, 15, in.nextLine());
+			validateNum(i, 15, edhData.in.nextLine());
 			
 			// card themes
 			System.out.print("Number of mana cards kept: ");
-			validateNum(i, 16, in.nextLine());
+			validateNum(i, 16, edhData.in.nextLine());
 			
 			System.out.print("Number of draw cards kept: ");
-			validateNum(i, 17, in.nextLine());
+			validateNum(i, 17, edhData.in.nextLine());
 			
 			System.out.print("Number of interaction cards kept: ");
-			validateNum(i, 18, in.nextLine());
+			validateNum(i, 18, edhData.in.nextLine());
 			
 			System.out.print("Number of threat cards kept: ");
-			validateNum(i, 19, in.nextLine());
+			validateNum(i, 19, edhData.in.nextLine());
 			
 			System.out.print("Number of combo cards kept: ");
-			validateNum(i, 20, in.nextLine());
+			validateNum(i, 20, edhData.in.nextLine());
 			
 			System.out.print("Number of other cards kept: ");
-			validateNum(i, 21, in.nextLine());
+			validateNum(i, 21, edhData.in.nextLine());
 			
 			// mulligan info
 			System.out.print("Number of mulligans: ");
-			validateNum(i, 22, in.nextLine());
+			validateNum(i, 22, edhData.in.nextLine());
 			int mulligans = Integer.parseInt(playerData[i][22]);
 			
 			// avoids asking player questions if there aren't mulligans
 			if (mulligans > 0) {
 				System.out.print("Number of cards pitched: ");
-				validateNum(i, 23, in.nextLine());
+				validateNum(i, 23, edhData.in.nextLine());
 				int pitched = Integer.parseInt(playerData[i][23]);
 				
 				if (pitched > 0) {
 					System.out.print("Total CMC of cards pitched: ");
-					validateNum(i, 24, in.nextLine());
+					validateNum(i, 24, edhData.in.nextLine());
 					
 					// card types
 					System.out.print("Number of artifacts pitched: ");
-					validateNum(i, 25, in.nextLine());
+					validateNum(i, 25, edhData.in.nextLine());
 					
 					System.out.print("Number of creatures pitched: ");
-					validateNum(i, 26, in.nextLine());
+					validateNum(i, 26, edhData.in.nextLine());
 					
 					System.out.print("Number of lands pitched: ");
-					validateNum(i, 27, in.nextLine());
+					validateNum(i, 27, edhData.in.nextLine());
 					
 					System.out.print("Number of enchantments pitched: ");
-					validateNum(i, 28, in.nextLine());
+					validateNum(i, 28, edhData.in.nextLine());
 					
 					System.out.print("Number of instants pitched: ");
-					validateNum(i, 29, in.nextLine());
+					validateNum(i, 29, edhData.in.nextLine());
 					
 					System.out.print("Number of sorceries pitched: ");
-					validateNum(i, 30, in.nextLine());
+					validateNum(i, 30, edhData.in.nextLine());
 					
 					System.out.print("Number of planeswalkers pitched: ");
-					validateNum(i, 31, in.nextLine());
+					validateNum(i, 31, edhData.in.nextLine());
 					
 					// card themes
 					System.out.print("Number of mana cards pitched: ");
-					validateNum(i, 32, in.nextLine());
+					validateNum(i, 32, edhData.in.nextLine());
 					
 					System.out.print("Number of draw cards pitched: ");
-					validateNum(i, 33, in.nextLine());
+					validateNum(i, 33, edhData.in.nextLine());
 					
 					System.out.print("Number of interaction cards pitched: ");
-					validateNum(i, 34, in.nextLine());
+					validateNum(i, 34, edhData.in.nextLine());
 					
 					System.out.print("Number of threat cards pitched: ");
-					validateNum(i, 35, in.nextLine());
+					validateNum(i, 35, edhData.in.nextLine());
 					
 					System.out.print("Number of combo cards pitched: ");
-					validateNum(i, 36, in.nextLine());
+					validateNum(i, 36, edhData.in.nextLine());
 					
 					System.out.print("Number of other cards pitched: ");
-					validateNum(i, 37, in.nextLine());
+					validateNum(i, 37, edhData.in.nextLine());
 				} else {
 					// for loop to assign 0 to all cards pitched
 					for (int j = 0; j < 14; j++) {
@@ -175,7 +166,7 @@ class dataCollection {
 			} // end of checks
 		
 			System.out.print("Did they scoop? y/n: ");
-			validateText(i, 38, in.nextLine(), yn);
+			validateText(i, 38, edhData.in.nextLine(), yn);
 			
 			// avoids asking about winning
 			if (playerData[i][38].equals("y") || winnerFound) {
@@ -183,30 +174,24 @@ class dataCollection {
 				playerData[i][40] = "-";
 			} else {
 				System.out.print("Did they win? y/n: ");
-				validateText(i, 39, in.nextLine(), yn);
+				validateText(i, 39, edhData.in.nextLine(), yn);
 				
 				// checks if a winner has been found
 				if (playerData[i][39].equals("y")) {
 					winnerFound = true;
 					
 					System.out.print("How did they win? aggro/aether/labman/combo/scoops/other: ");
-					validateText(i, 40, in.nextLine(), winCons);
+					validateText(i, 40, edhData.in.nextLine(), winCons);
 				} else {
 					playerData[i][40] = "-";
 				} //end of checks
 			} //end of checks
 			
-			System.out.println();
-			System.out.println();
-			System.out.println();
 			System.out.println(edhData.divider);
-			System.out.println();
-			System.out.println();
-			System.out.println();
 		} // end of for loop
 		
 		System.out.println("Game data collection finished.");
-		in.close();
+		System.out.println(edhData.divider);
 	} // end of function collectData
 	
 	/*
@@ -214,7 +199,6 @@ class dataCollection {
 	 * makes sure that user input is the type that can be used
 	 */
 	private void validateText(int i, int j, String input, String[] options) {
-		System.out.println();
 		
 		// while loop to make sure the user inputs correct data
 		while(true) {
@@ -241,8 +225,7 @@ class dataCollection {
 				} // end of check
 			} // end of for loop
 			
-			input = in.nextLine();
-			System.out.println();
+			input = edhData.in.nextLine();
 		} // end of while loop
 		
 	} // end of function validate
@@ -252,7 +235,6 @@ class dataCollection {
 	 * makes sure the user gives a number
 	 */
 	private void validateNum(int i, int j, String input) {
-		System.out.println();
 		
 		// while loop to make sure user inputs an integer
 		while(true) {
@@ -262,8 +244,7 @@ class dataCollection {
 				return;
 			} catch(NumberFormatException e) {
 				System.out.print("Please input a number: ");
-				input = in.nextLine();
-				System.out.println();
+				input = edhData.in.nextLine();
 			} //end of check
 		} // end of while loop
 	} // end of method validateNum
--- src/edhData.java
+++ src/edhData.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 // edhData class by Garrett Chapman
@@ -5,13 +6,14 @@ import java.util.Scanner;
 
 public class edhData {
 	// constructs
-	private static Scanner in = new Scanner(System.in);
+	public static Scanner in = new Scanner(System.in);
 	
 	// menu text
 	public static String divider = "===========================================================================================================================================================";
 	private static String[] commands = {
-			"help",
-			"play"
+			"add",
+			"exit",
+			"help"
 	};
 	
 	/*
@@ -23,7 +25,30 @@ public class edhData {
 		
 		// while loop to allow functionality
 		while(true) {
-			
+			commands();
+			try {
+				System.out.println("Please enter a command: ");
+				switch(in.nextLine()) {
+				case "add":
+					System.out.println(divider);
+					add();
+					break;
+				case "exit":
+					System.out.println(divider);
+					System.out.println("Program closing.  Thank you.");
+					System.out.println(divider);
+					System.exit(0);
+					break;
+				case "help":
+					System.out.println(divider);
+					break;
+				default:
+					System.out.println(divider);
+					System.out.println("ERROR: unknown command.");
+				}
+			} catch(NoSuchElementException e) {
+				System.out.println("ERROR: bad scan");
+			}
 		} // end of while loop
 	} // end of main method
 	
@@ -37,7 +62,7 @@ public class edhData {
 		System.out.println("This software is designed to keep track of EDH (Commander) games of Magic: The Gathering, which is created by Wizards of the Coast and not owned by me.");
 		System.out.println("See the github repository here: https://github.com/garrettschapman/edhData");
 		
-		commands();
+		System.out.println(divider);
 	} //end of function mainMenu
 	
 	/*
@@ -45,26 +70,24 @@ public class edhData {
 	 * separated from main menu function because it will be printed again for incorrect input and after a command
 	 */
 	private static void commands() {
-		System.out.println(divider);
-		
 		System.out.println("Known commands:");
 		// for loop to add in commands
 		for (int i = 0; i < commands.length; i++) {
 			System.out.print("     " + commands[i]);
 		} // end of for loop
 		System.out.println();
-		
-		System.out.println(divider);
-		
-		System.out.println("enter a command: ");
+		System.out.println();
 	} // end of function commands
+	
+	
 	
 	/*
 	 *  function to collect and store game data
+	 *  called by add command
 	 */
-	private static void gameData() {
+	private static void add() {
 		System.out.print("How many players? ");
-		int players = in.nextInt();
+		int players = Integer.parseInt(in.nextLine());
 		System.out.println();
 		
 		// collects data from the game
@@ -76,5 +99,5 @@ public class edhData {
 		store.storeData(collect.getPlayerData());
 		
 		commands();
-	} //end of function gameData
+	} //end of function add
 } //end of class edhData
