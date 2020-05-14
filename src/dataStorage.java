165a061e06ea94b587a04ba1b3540e07881d2fcc
Garrett Chapman <36682034+garrettschapman@users.noreply.github.com>
2020-05-13 23:53:30 -0400

    Continued work on finding differences

--- src/dataStorage.java
+++ src/dataStorage.java
@@ -35,7 +35,30 @@ class dataStorage {
 	private void makeNewPlayers(String[][] data) {
 		newPlayers = new Object[data.length][45];
 		
-		
+		// for loop to fill new players
+		for(int i = 0; i < newPlayers.length; i++) {
+			newPlayers[i][0] = 0;			// player ID (blank)
+			newPlayers[i][1] = data[i][0];	// player name
+			newPlayers[i][2] = 1;			// game (add a game)
+			
+			switch(data[i][3]) {			// fun/eh/unfun
+			case "y":
+				newPlayers[i][3] = 1;
+				newPlayers[i][4] = 0;
+				newPlayers[i][5] = 0;
+			case "e":
+				newPlayers[i][3] = 0;
+				newPlayers[i][4] = 1;
+				newPlayers[i][5] = 0;
+			case "n":
+				newPlayers[i][3] = 0;
+				newPlayers[i][4] = 0;
+				newPlayers[i][5] = 1;
+			}
+			
+			newPlayers[i][6] = i;
+			
+		} // end of for loop
 	} // end of function makeNewPlayers
 	
 	// find differences
